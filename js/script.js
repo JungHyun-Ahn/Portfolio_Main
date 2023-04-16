2b505cf6a3959d5f1b2808388fd1aff0aae965e2
JungHyun-Ahn <84699689+JungHyun-Ahn@users.noreply.github.com>
2023-04-16 23:24:17 +0900

    Update script.js
    scroll시 nav에 boder-bottom 추가
    scroll 시 해당 영역에 맞는 nav에 active class를 추가
    scroll 시 animation을 추가해보려 노력함......(구현 안됨)

--- js/script.js
+++ js/script.js
@@ -3,8 +3,44 @@
 //   $('.works-item').load('/includes/work-items.html');
 // })
 
-// Design Menu List
-const worksBtnContainer = document.querySelector(".works-items-menu"); 
+// == Main Navigation Scroll Event ==
+const winHeight = window.innerHeight;
+const mainNav = document.querySelector(".main-nav");
+const mainNavList = document.querySelectorAll(".main-nav ul li");
+const articles = document.querySelectorAll("article");
+const articlesTitle = document.querySelectorAll("article .sub-title");
+
+window.addEventListener("scroll", () => {
+  if(scrollY > 0) {
+    mainNav.classList.add("active");
+  } else {
+    mainNav.classList.remove("active");
+  }
+  for(let i = 0; i < mainNavList.length; i++) {
+    if(scrollY >= articles[i].offsetTop - 80 ) {
+      for(let j = 0; j < mainNavList.length; j++) {
+        mainNavList[j].classList.remove("active");
+      }
+      mainNavList[i].classList.add("active");
+    }
+    // articlesTitle[i].classList.add("active");
+  }
+  // if(scrollY > articles[1].offsetTop - winHeight + 500) {
+  //   console.log(scrollY);
+  //   console.log(articles[1].offsetTop);
+  //   console.log(winHeight);
+  // }
+});
+
+// == Scroll Animation ==
+// const winHeight = window.innerHeight;
+// console.log(winHeight);
+// window.addEventListener("scroll", () => {
+  
+// });
+
+// == Design Menu List ==
+const worksBtnContainer = document.querySelector(".works-items-menu.design"); 
 const items = document.querySelectorAll(".item");
 
 worksBtnContainer.addEventListener('click', function(e) {
@@ -13,7 +49,7 @@ worksBtnContainer.addEventListener('click', function(e) {
     return;
   }
 
-  const active = document.querySelector('.works-items-menu-list.active');
+  const active = document.querySelector('.design .works-items-menu-list.active');
   active.classList.remove('active');
   const target = e.target.nodeName === 'li' ? e.target : e.target.parentNode;
   target.classList.add('active');
